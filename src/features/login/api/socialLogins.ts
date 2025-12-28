2bc1d7ae9473926bb140f29f3e688f7714979bb0
mtm1018 <mtm1018@naver.com>
2025-12-28 13:22:42 +0900

    docs/문서추가

--- src/features/login/api/socialLogins.ts
+++ src/features/login/api/socialLogins.ts
@@ -5,7 +5,7 @@ export const kakaoLogin = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'kakao',
     options: {
-      redirectTo: 'http://localhost:3000/auth/callback',
+      redirectTo: `${window.location.origin}/auth/callback`,
     },
   });
   if(data) alert('로그인 성공')
@@ -15,12 +15,12 @@ export const kakaoLogin = async () => {
 
 
 export const googleLogin = async() => {
-  const { data, error } = await supabase.auth.signInWithOAuth({
+  const {  error } = await supabase.auth.signInWithOAuth({
     provider: 'google',
       options: {
-      redirectTo: 'http://localhost:3000/auth/callback',
+      redirectTo: `${window.location.origin}/auth/callback`,
     },
   })
-  if (data) console.log(data)
+
   if (error) throw new Error('구글 소셜로그인 실패')
 }
